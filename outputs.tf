c9fda1b5d91143057106643d98134dae96235197
alhaj1986 <105738799+alhaj1986@users.noreply.github.com>
2024-07-04 21:27:15 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,9 @@ output "private_subnets" {
 output "autoscaling_group_name" {
   value = aws_autoscaling_group.app.name
 }
+
+output "private_key_pem" {
+  description = "The private key in PEM format, base64 encoded"
+  value       = tls_private_key.example.private_key_pem
+  sensitive   = true
+}
