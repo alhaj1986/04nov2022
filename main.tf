e81d714e75317107ae95a6c9be71025a2fea599c
alhaj1986 <105738799+alhaj1986@users.noreply.github.com>
2024-07-04 21:23:41 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,97 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "3.14.2"
+
+  name = var.vpc_name
+  cidr = "10.0.0.0/16"
+  
+  azs             = ["us-west-2a", "us-west-2b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+  
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_launch_template" "app" {
+  name_prefix   = "app-launch-template"
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              sudo apt-get install -y tomcat9
+              EOF
+
+  key_name = var.key_name
+
+  network_interfaces {
+    associate_public_ip_address = true
+    subnet_id                   = element(module.vpc.public_subnets, 0)
+    security_groups             = [aws_security_group.instance.id]
+  }
+}
+
+resource "aws_autoscaling_group" "app" {
+  desired_capacity     = 2
+  max_size             = 5
+  min_size             = 2
+  launch_template {
+    id      = aws_launch_template.app.id
+    version = "$Latest"
+  }
+  vpc_zone_identifier = module.vpc.public_subnets
+
+  tag {
+    key                 = "Name"
+    value               = "Tomcat-Server"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name        = "instance_sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-20.04-amd64-server-*"]
+  }
+}
