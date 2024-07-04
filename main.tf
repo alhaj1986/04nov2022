4b54455c2fd3524a1a2b59b28924d978092b79d2
alhaj1986 <105738799+alhaj1986@users.noreply.github.com>
2024-07-04 22:00:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,9 +10,7 @@ terraform {
 }
 
 provider "aws" {
-  region     = "us-west-2"
-  access_key = "AWS_ACCESS_KEY_ID"
-  secret_key = "AWS_SECRET_ACCESS_KEY"
+  region = "us-west-2"
 }
 
 resource "aws_instance" "app_server" {
