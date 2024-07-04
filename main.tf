064beb4fffb72a375535d201bd0d5500923dd044
alhaj1986 <105738799+alhaj1986@users.noreply.github.com>
2024-07-04 22:14:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,22 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS access key"
+  type        = string
+  sensitive   = true
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS secret key"
+  type        = string
+  sensitive   = true
+}
+
 provider "aws" {
-  region = "us-west-2"
+  region     = "us-west-2"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 resource "aws_instance" "app_server" {
